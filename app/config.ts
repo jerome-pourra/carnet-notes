1ea5c6bc41f9fc0ccbe21a032742c3ab5197825a
Jerome Pourra <j.pourra.perso@gmail.com>
2025-09-19 16:12:50 +0200

    reset config

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
-const HOST = '192.168.1.74'; // Remplacez par votre IP privée
+const HOST = 'VOTRE_IP_PRIVE'; // Remplacez par votre IP privée
 const PORT = 3000; // Port par défaut de JSON Server (ne pas changer ou alors... adapter avec le script dans package.json)
 
 export const BACKEND_URL = `http://${HOST}:${PORT}`;
